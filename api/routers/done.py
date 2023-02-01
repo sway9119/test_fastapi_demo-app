961fa2451424d1877ed5ab84e9c1bb17b2433313
sway <40759948+sway9119@users.noreply.github.com>
2023-02-01 20:38:16 +0900

    Update done.py

--- api/routers/done.py
+++ api/routers/done.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.put("/tasks/{task_id}/done")
-async def mark_task_as_done():
-  pass
+async def mark_task_as_done(task_id: int):
+  return
 
 @router.delete("/tasks/{task_id}/done")
-async def unmark_task_as_done():
-  pass
+async def unmark_task_as_done(task_id: int):
+  return
 
