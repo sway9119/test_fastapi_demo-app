b7fff023af584cc7fc1ea59c311eefcb352a7a6b
sway <40759948+sway9119@users.noreply.github.com>
2023-03-01 22:41:39 +0900

    db名が競合するので変更

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from api.models.task import Base
 
-DB_URL = "mysql+pymysql://root@db3306/demo?charset=utf8"
+DB_URL = "mysql+pymysql://root@x-db3306/demo?charset=utf8"
 engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
   Base.metadata.drop_all(bind=engine)
   Base.metadata.create_all(bind=engine)
 
-  if __name__ == "__main__":
-    reset_database()
+if __name__ == "__main__":
+  reset_database()
