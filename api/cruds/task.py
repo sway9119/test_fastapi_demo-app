db140898ad4ffeddaae587018257d3f2362333dc
sway <40759948+sway9119@users.noreply.github.com>
2023-03-04 16:26:09 +0900

    Readの実装

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 # async def関数んが非同期処理を行う子ルーチン関数として定義
 async def create_task(
@@ -15,3 +17,16 @@ async def create_task(
   # DB上のデータを元にTackインスタンスを更新する（この場合、作成したレコードのidを取得する）
   await db.refresh(task)
   return task
+
+async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+  result: Result = await (
+    db.execute(
+        select(
+          task_model.Task.id,
+          task_model.Task.title,
+          task_model.Done.id.isnot(None).label("done"),
+        ).outerjoin(task_model.Done)
+      )
+  )
+  return result.all()
+  
